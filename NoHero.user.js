0c4a51475d5cfb74a7489fb9283843ae56e532f1
Adrian Cornish <github@bluedreamer.com>
2024-04-16 12:29:45 -0600

    Update

--- NoHero.user.js
+++ NoHero.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         NoHero
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-16
-// @description  try to take over the world!
-// @author       You
+// @version      1.0.1
+// @description  Removed annoying hero image
+// @author       bluedreamer
 // @match        https://app.scorechaser.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=scorechaser.com
+// @downloadURL  https://github.com/bluedreamer/tampermonkey/raw/master/NoHero.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -14,5 +15,5 @@
     const style = document.createElement('style');
     style.textContent = 'div.home-hero-image {display: none;}';
     document.head.appendChild(style);
-    
+
 })();
