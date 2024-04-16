2c8ae63b5a4a31fb3f6ad7601cc2fdefe18b2504
Adrian Cornish <github@bluedreamer.com>
2024-04-16 12:33:17 -0600

    Update reddit script

--- Reddit fixer.user.js
+++ Reddit fixer.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name Reddit fixer
+// @name         Reddit fixer
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-10
-// @description  try to take over the world!
-// @author       You
+// @version      1.0.1
+// @description  Full images and remove "see full image"
+// @author       bluedreamer
 // @match        https://www.reddit.com/*
 // @match        https://new.reddit.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @downloadURL  https://github.com/bluedreamer/tampermonkey/raw/master/Reddit%20fixer.user.js
 // @grant        none
 // ==/UserScript==
 
